656ad3cd03d364c75aa45f4e7d8d3bae35f2404e
d0t-J <j.ahmed.ja7a7@gmail.com>
2025-01-12 21:55:23 +0500

    README.md in main

--- DumDum.cpp
+++ DumDum.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void increment(int &num) {  // & indicates pass-by-reference
+void increment(int &num)
+{ // & indicates pass-by-reference
     num += 1;
     cout << "Inside function: " << num << endl;
 }
 
-int main() {
+int main()
+{
     int value = 10;
-    increment(value);  // Pass-by-reference
-    cout << "Outside function: " << value << endl;  // Output: 11
+    increment(value);                              // Pass-by-reference
+    cout << "Outside function: " << value << endl; // Output: 11
     return 0;
 }
